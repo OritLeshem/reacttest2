08b4959725efef14d71a3594280d7421f9e5d1e0
OritLeshem <azmono@gmail.com>
2021-08-24 22:22:59 -0400

    hello again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>hello</h1>
+        <h1>hello again hello again hello again</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
